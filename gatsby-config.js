14232d8b7b7408d6e08395fd66db32a786ba06a7
undigitals <undigitals@gmail.com>
2020-05-03 23:10:10 +0900

    forced arg

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
     social: [
       {
         name: `github`,
-        url: `https://github.com/codebushi/gatsby-theme-document`
+        url: `https://github.com/codebushi/gatsby-theme-document`,
       },
       {
         name: `twitter`,
-        url: `https://twitter.com/HuntaroSan`
-      }
+        url: `https://twitter.com/HuntaroSan`,
+      },
     ],
     sidebarConfig: {
-      forcedNavOrder: ["/introduction", "/codeblock"],
-      ignoreIndex: true
-    }
+      forcedNavOrder: ["/introduction", "/arbitrary-argument", "/codeblock"],
+      ignoreIndex: true,
+    },
   },
-  plugins: [{ resolve: `gatsby-theme-document` }]
+  plugins: [{ resolve: `gatsby-theme-document` }],
 };
